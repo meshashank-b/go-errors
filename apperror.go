92120562b79024e6e664fafb0fbbc1c0e934c4a9
shashank <shashankba13@proton.me>
2025-03-05 11:13:57 +0530

    add the stack trace info and implement erro and unwrap interface for apperror

--- apperror.go
+++ apperror.go
@@ -1,12 +1,17 @@
 package apperror
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type AppError struct {
 	Code       string      `json:"code"`
 	Message    string      `json:"message"`
 	HTTPStatus int         `json:"http_status"`
 	Details    interface{} `json:"details,omitempty"`
-	StackTrace []string    `json:"stack_trace,omitempty"`
-	Err        error       `json:"-"`
+	stackTrace *stackTrace
+	Err        error `json:"-"`
 }
 
 func NewAppError(code string, message string, httpStatus int, err error) *AppError {
@@ -14,7 +19,37 @@ func NewAppError(code string, message string, httpStatus int, err error) *AppErr
 		Code:       code,
 		Message:    message,
 		HTTPStatus: httpStatus,
-		StackTrace: captureStackTrace(),
+		stackTrace: captureStackTrace(),
 		Err:        err,
 	}
+}
+
+func (e *AppError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("[%s] %s: %v", e.Code, e.Message, e.Err)
+	}
+	return fmt.Sprintf("[%s] %s", e.Code, e.Message)
+}
+
+func (e *AppError) Unwrap() error {
+	return e.Err
+}
+
+// StackTrace returns the stack trace associated with the AppError.
+// It provides detailed information about the sequence of function calls
+// that led to the error, which can be useful for debugging purposes.
+func (e *AppError) StackTrace() *stackTrace {
+	return e.stackTrace
+}
+
+func (e *AppError) MarshalJSON() ([]byte, error) {
+	type Alias AppError // Avoid infinite recursion
+
+	return json.Marshal(&struct {
+		*Alias
+		StackTrace *stackTrace `json:"stack_trace,omitempty"`
+	}{
+		Alias:      (*Alias)(e),
+		StackTrace: e.stackTrace,
+	})
 }
--- stacktrace.go
+++ stacktrace.go
@@ -1,22 +1,78 @@
 package apperror
 
 import (
-	"fmt"
+	"encoding/json"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
-func captureStackTrace() []string {
+// stackTrace represents a series of stack frames collected during an error.
+type stackTrace struct {
+	Frames []stackFrame `json:"frames"`
+}
+
+// stackFrame represents a single frame in a stack trace.
+type stackFrame struct {
+	Function        string  `json:"function"`
+	File            string  `json:"file"`
+	Line            int     `json:"line"`
+	Pointer         uintptr `json:"pointer"`
+	FunctionPointer uintptr `json:"function_pointer"`
+}
+
+// captureStackTrace collects the current stack trace, starting from the caller of this function.
+func captureStackTrace() *stackTrace {
 	var pcs [32]uintptr
 	n := runtime.Callers(3, pcs[:])
-	frames := runtime.CallersFrames(pcs[:n])
 
-	var trace []string
-	for {
-		frame, more := frames.Next()
-		trace = append(trace, fmt.Sprintf("%s:%d %s", frame.File, frame.Line, frame.Function))
-		if !more {
-			break
+	frames := make([]stackFrame, 0, n) // Preallocate slice to reduce reallocations
+
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		if fn != nil {
+			file, line := fn.FileLine(pc)
+			frames = append(frames, stackFrame{
+				Function:        fn.Name(),
+				File:            file,
+				Line:            line,
+				Pointer:         pc,
+				FunctionPointer: fn.Entry(),
+			})
+		} else {
+			frames = append(frames, stackFrame{
+				Function:        "unknown",
+				File:            "unknown",
+				Line:            0,
+				Pointer:         pc,
+				FunctionPointer: 0,
+			})
 		}
 	}
-	return trace
+	return &stackTrace{Frames: frames}
+}
+
+// String returns a formatted string representation of the stack trace.
+func (st *stackTrace) String() string {
+	var sb strings.Builder
+	sb.WriteString("Stack Trace:\n---------------------------------------------------\n")
+	for i, frame := range st.Frames {
+		sb.WriteString("#" + strconv.Itoa(i+1) + " - Function: " + frame.Function + "\n")
+		sb.WriteString("     Location: " + frame.File + ":" + strconv.Itoa(frame.Line) + "\n")
+		sb.WriteString("     Pointer: 0x" + strconv.FormatUint(uint64(frame.Pointer), 16) + "\n")
+		sb.WriteString("     Function Pointer: 0x" + strconv.FormatUint(uint64(frame.FunctionPointer), 16) + "\n")
+		sb.WriteString("---------------------------------------------------\n")
+	}
+	return sb.String()
+}
+
+// enhanceWithCause merges another stack trace into the current one immutably.
+func (st *stackTrace) enhanceWithCause(original *stackTrace) *stackTrace {
+	newFrames := append(st.Frames, original.Frames...)
+	return &stackTrace{Frames: newFrames}
+}
+
+// MarshalJSON provides a pretty JSON format for the stack trace.
+func (st *stackTrace) MarshalJSON() ([]byte, error) {
+	return json.MarshalIndent(st, "", "  ")
 }
